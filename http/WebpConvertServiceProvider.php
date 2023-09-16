52815290b80687261fb0657265909a46c2a6b740
Halil İbrahim BELKIR <ibrahim.belkir@generali.com.tr>
2023-09-16 16:41:00 +0300

    deneme

---
+++ http/WebpConvertServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Joy\VoyagerDuplicate;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class VoyagerDuplicateServiceProvider
+ *
+ * @category  Package
+ * @package   JoyVoyagerDuplicate
+ * @author    Ramakant Gangwar <gangwar.ramakant@gmail.com>
+ * @copyright 2021 Copyright (c) Ramakant Gangwar (https://github.com/rxcod9)
+ * @license   http://github.com/rxcod9/joy-voyager-duplicate/blob/main/LICENSE New BSD License
+ * @link      https://github.com/rxcod9/joy-voyager-duplicate
+ */
+class WebpConvertServiceProvider extends ServiceProvider
+{
+
+    protected function registerPublishables(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../config/voyager-duplicate.php' => config_path('img-webp-convert.php'),
+        ], 'config');
+    }
+}
